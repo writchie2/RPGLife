702cd39acdf9f7917ed696fb9a434df96de8e53a
writchie2 <writchie@uwm.edu>
2025-02-12 18:28:07 -0600

    Made Account registration require username, email, birthday, and password that has 8 chars, upper, lower, number, and special char. When account is created a doc is created in database for user.

--- app/(login)/register.tsx
+++ app/(login)/register.tsx
@@ -2,9 +2,7 @@
 * Where a user can create an account
 *
 * TODO: 
-* Move register account to own page.
 * Add alternate register options
-* Expand current options to ask for more info (name, phone number)
 * Style and logo
 * 
 */
@@ -12,8 +10,11 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'fire
 import { router } from 'expo-router'
 import { Text, StyleSheet, TextInput, TouchableOpacity, SafeAreaView } from 'react-native'
 import { auth } from '../../FirebaseConfig'
+import { validatePassword } from 'firebase/auth'
 import React, { useState } from 'react'
 import { db } from '../../FirebaseConfig'
+import { setDoc, doc, query, where } from 'firebase/firestore';
+import DatePickerComponent from "../../components/DatePickerComponent";
 
 import { useEffect } from 'react';
 import { BackHandler } from 'react-native';
@@ -30,26 +31,39 @@ export default function RegisterScreen() {
         return () => backHandler.remove(); // Cleanup
       }, []);
     
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+    const [userName, setUserName] = useState('');
+    //const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
+    const [date, setDate] = useState(new Date());
+    const [dateSelected, setDateSelected] = useState(false);
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+
+    
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,}$/;
+
   
 const signUp = async () => {
 
-    if(firstName.trim().length === 0 || lastName.trim().length || email.trim().length )
+    if(userName.trim().length === 0  || email.trim().length ===0 || password.trim().length === 0 || !dateSelected)
     {
       alert('Please fill out all fields!');
       return;
     }
-    if (password === confirmPassword)
+    if (!(password === confirmPassword))
     {
       alert('Passwords do not match!');
       return;
     }
-    // Add some regex logic here for password complexity. 
-    // allow these chars 0-9A-Za-z/_-!, 8 chars, etc
+    const status = await validatePassword(auth, password);
+    if (!status.isValid) {
+      alert('Password does not meet requirements!');
+      // This stuff we can use later to show a more detailed message
+      const needsLowerCase = status.containsLowercaseLetter !== true;
+      const needsUpperCase = status.containsUppercaseLetter !== true;
+      const needsNumericCase = status.containsNumericCharacter !== true;
+      const needsSpecialCase = status.containsNonAlphanumericCharacter !== true;
+    }
 
     try {
       const user = await createUserWithEmailAndPassword(auth, email, password)
@@ -57,6 +71,18 @@ const signUp = async () => {
       {
         
         alert("Account created succesfully!");
+        if(auth.currentUser?.uid){
+          alert('we got here' + auth.currentUser.uid);
+          await setDoc(doc(db, "users", auth.currentUser.uid), {
+            username: userName,
+            email: email,
+            birthday: date.toDateString()
+          });
+          
+      }
+      else {
+        alert("Database did not create a new doc for new user");
+          }
         auth.signOut();
         router.replace('/(login)');
       }
@@ -69,11 +95,17 @@ const signUp = async () => {
   return (
       <SafeAreaView style={styles.container}>
         <Text style={styles.title}>Create a new account</Text>
-        <TextInput style={styles.textInput} placeholder="First Name" value={firstName} onChangeText={setFirstName} />
-        <TextInput style={styles.textInput} placeholder="Last Name" value={lastName} onChangeText={setLastName} />
-        <TextInput style={styles.textInput} placeholder="Email" autoCapitalize='none' value={email} onChangeText={setEmail} />
-        <TextInput style={styles.textInput} placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry/>
-        <TextInput style={styles.textInput} placeholder="Confirm Password" value={confirmPassword} onChangeText={setConfirmPassword} secureTextEntry/>
+        
+        <TextInput style={styles.textInputFull} placeholder="User Name" placeholderTextColor={'#D3D3D3'} autoCapitalize='none' value={userName} onChangeText={setUserName} />
+  
+        <SafeAreaView style={styles.row}>
+          <TextInput style={styles.textInputRow} placeholder="Email" placeholderTextColor={'#D3D3D3'} autoCapitalize='none' value={email} onChangeText={setEmail} />
+          <DatePickerComponent style={styles.textInputRow} label="Birthday" dateSelected={dateSelected} onDateChange={(date: Date) => {setDate(date); setDateSelected(true); }}/>
+        </SafeAreaView>
+        <SafeAreaView style={styles.row}>
+          <TextInput style={styles.textInputRow} placeholder="Password" placeholderTextColor={'#D3D3D3'} value={password} onChangeText={setPassword} secureTextEntry/>
+          <TextInput style={styles.textInputRow} placeholder="Confirm Password" placeholderTextColor={'#D3D3D3'} value={confirmPassword} onChangeText={setConfirmPassword} secureTextEntry/>
+        </SafeAreaView>
         <TouchableOpacity style={styles.button} onPress={signUp}>
           <Text style={styles.text}>Create</Text>
         </TouchableOpacity>
@@ -89,15 +121,38 @@ const signUp = async () => {
       alignItems: 'center',
       backgroundColor: '#FAFAFA', // A softer white for a modern, minimalist background
     },
+    row: {
+      flexDirection: 'row',
+      justifyContent: 'space-between', // Optional, for spacing between inputs
+      padding: 10,
+      gap: '05%',
+    },
     title: {
       fontSize: 28, // A bit larger for a more striking appearance
       fontWeight: '800', // Extra bold for emphasis
       marginBottom: 40, // Increased space for a more airy, open feel
       color: '#1A237E', // A deep indigo for a sophisticated, modern look
     },
-    textInput: {
+    textInputFull: {
       height: 50, // Standard height for elegance and simplicity
-      width: '90%', // Full width for a more expansive feel
+      width: '85%', // Full width for a more expansive feel
+      backgroundColor: '#FFFFFF', // Pure white for contrast against the container
+      borderColor: '#E8EAF6', // A very light indigo border for subtle contrast
+      borderWidth: 2,
+      borderRadius: 15, // Softly rounded corners for a modern, friendly touch
+      marginVertical: 15,
+      paddingHorizontal: 25, // Generous padding for ease of text entry
+      fontSize: 16, // Comfortable reading size
+      color: '#3C4858', // A dark gray for readability with a hint of warmth
+      shadowColor: '#9E9E9E', // A medium gray shadow for depth
+      shadowOffset: { width: 0, height: 4 },
+      shadowOpacity: 0.3,
+      shadowRadius: 4,
+      elevation: 4, // Slightly elevated for a subtle 3D effect
+    },
+    textInputRow: {
+      height: 50, // Standard height for elegance and simplicity
+      width: '40%', // Full width for a more expansive feel
       backgroundColor: '#FFFFFF', // Pure white for contrast against the container
       borderColor: '#E8EAF6', // A very light indigo border for subtle contrast
       borderWidth: 2,
