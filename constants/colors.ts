4c2aeb6c28917ee0a553090f26439f472ed6f156
djsdesigns <104704207+djsdesigns@users.noreply.github.com>
2025-03-06 18:23:36 -0600

    Update colors.ts

--- constants/colors.ts
+++ constants/colors.ts
@@ -3,9 +3,23 @@ const tintColorLight = "#2f95dc";
 const tintColorDark = "#fff";
 
 // Backgrounds
+// const bgMain = "#f1f3de";
+// const bgMain = "#e7ecda";
 // const bgPrimary = "#E4EAD6";
-const bgPrimary = "#e7ecda"; // 10% lighter
+// const bgPrimary = "#e7ecda"; // 10% lighter (make dropdown and go with lighter color, make tertiary = secondary?)
+const bgPrimary = "#f1f3de"; // even lighter
 const bgSecondary = "#C2CFA0";
+// const bgTertiary = "#C2C8A0"; // test lighter
+// const bgTertiary = "#8E9970E6"; // 90% oppacity
+// const bgTertiary = "#8E9970CC"; // 80% oppacity
+// const bgTertiary = "#99a47a"; // color droppered to get color w/o opacity
+// const bgTertiary = "#99a67e"; // Test
+// const bgTertiary = "#9ba77d"; // Best?!
+const bgTertiary = "#C2CFA0"; // Secondary
+// const bgDropdown = "#C2CFA0";
+// const bgDropdown = "#C9D5AB";
+// const bgDropdown = "#eaf3d3";
+const bgDropdown = "#f0f7d7";
 
 // Text
 const text = "#394022E6"; // 90% opacity
@@ -16,7 +30,10 @@ const textLight = "#394022CC"; // 80% opacity
 const textInput = "#394022CC";
 // const textPlaceholder = "#39402260"; // ~38% opacity
 const textPlaceholder = "#39402266"; // 40% opacity
+
+// Borders
 const borderInput = "#656E4A";
+const borderLight = "#656E4A80"; // 50% opacity
 
 // Shadows
 const shadow = "#555";
@@ -39,8 +56,11 @@ export default {
     tabIconSelected: tintColorDark,
   },
   // Backgrounds:
+  // bgMain,
   bgPrimary,
   bgSecondary,
+  bgTertiary,
+  bgDropdown,
   // Text:
   text,
   textDark,
@@ -48,7 +68,9 @@ export default {
   // Input fields:
   textInput,
   textPlaceholder,
+  // Borders:
   borderInput,
+  borderLight,
   // Shadows:
   shadow,
   shadowLight,
