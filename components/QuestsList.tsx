7f367c4df29a2480a5cfbc4ea4a479f13c13c093
djsdesigns <104704207+djsdesigns@users.noreply.github.com>
2025-03-06 18:29:15 -0600

    Update QuestsList.tsx
    added styling

--- components/QuestsList.tsx
+++ components/QuestsList.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
-import { Quest } from '../utils/types';
+import { Quest } from "../utils/types";
+import colors from "@/constants/colors";
 
 interface QuestsListProps {
   quests: Quest[];
@@ -9,95 +16,121 @@ interface QuestsListProps {
 }
 
 //Component that returns a Flat List with quests from a given quest array
-const QuestsList: React.FC<QuestsListProps> = ({ 
+const QuestsList: React.FC<QuestsListProps> = ({
   quests, // Array of quests from UserData interface
-  mode // "active", "inactive", or "all"
+  mode, // "active", "inactive", or "all"
 }) => {
-  
   //Item that will be rendered for each quest
   //TO DO: styling
   //TO DO: change press action to route to quest page
   const renderItem = ({ item }: { item: Quest }) => (
-    <TouchableOpacity style={styles.questItem} onPress={() => alert("You selected quest with id:" + item.id)}>
+    <TouchableOpacity
+      style={styles.questItem}
+      onPress={() => alert("You selected quest with id:" + item.id)}
+    >
       <Text style={styles.questName}>{item.name}</Text>
       <Text style={styles.questDescription}>{item.description}</Text>
       <Text style={styles.questDetails}>
         Difficulty: {item.difficulty} | Due: {item.dueDate}
       </Text>
       <Text style={styles.questSkills}>
-        Primary Skill: {item.primarySkill} {item.secondarySkill ? "| Secondary Skill: " + item.secondarySkill : '' }
+        Primary Skill: {item.primarySkill}{" "}
+        {item.secondarySkill ? "| Secondary Skill: " + item.secondarySkill : ""}
       </Text>
       <Text style={styles.questRepeatability}>
-        Repeatable: {item.repeatable ? 'Yes' : 'No'}
+        Repeatable: {item.repeatable ? "Yes" : "No"}
       </Text>
     </TouchableOpacity>
   );
 
   let chosenQuests: Quest[] = quests;
-  if (mode === "active"){
+  if (mode === "active") {
     chosenQuests = quests.filter((quest) => quest.active);
-  }
-  else if(mode === "inactive"){
+  } else if (mode === "inactive") {
     chosenQuests = quests.filter((quest) => !quest.active);
   }
 
   //If no quests are chosen, output text.
   if (chosenQuests.length == 0) {
-    return(
-    <View style={styles.questItem}>
-      <Text>No quests available</Text>
-    </View>
+    return (
+      <View style={styles.questItem}>
+        <Text>No quests available</Text>
+      </View>
     );
   }
 
   return (
-    <FlatList
-      data={chosenQuests}
-      keyExtractor={(item) => item.id}
-      renderItem={renderItem}
-      contentContainerStyle={styles.listContainer}
-    />
+    <View style={styles.list}>
+      <FlatList
+        data={chosenQuests}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        contentContainerStyle={styles.listContainer}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  listContainer: {
-    padding: 16,
-  },
-  questItem: {
-    padding: 10,
-    marginBottom: 12,
+  list: {
+    position: "relative",
+    top: -60,
+    marginBottom: -60,
+    zIndex: 0,
     borderRadius: 8,
-    backgroundColor: '#f0f0f0',
-    shadowColor: '#000',
+    width: "100%",
+    paddingTop: 50,
+    backgroundColor: colors.bgDropdown,
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 5,
     elevation: 3,
   },
+  listContainer: {
+    padding: 16,
+    paddingBottom: 12,
+  },
+  questItem: {
+    padding: 10,
+    marginBottom: 12,
+    backgroundColor: "transparent",
+    borderBottomWidth: 1,
+    // borderLeftWidth: 1,
+    borderColor: colors.borderLight,
+  },
   questName: {
-    fontSize: 18,
-    fontWeight: 'bold',
+    fontFamily: "Metamorphous_400Regular",
+    fontSize: 20,
+    color: colors.text,
   },
   questDescription: {
+    // fontFamily: "Alegreya_400Regular",
+    fontFamily: "Alegreya_500Medium",
     marginTop: 5,
-    fontSize: 14,
-    color: '#555',
+    fontSize: 16,
+    color: colors.textLight,
   },
   questDetails: {
+    // fontFamily: "Alegreya_400Regular",
+    fontFamily: "Alegreya_500Medium",
     marginTop: 5,
-    fontSize: 12,
-    color: '#888',
+    fontSize: 14,
+    color: colors.textLight,
   },
   questSkills: {
+    // fontFamily: "Alegreya_400Regular",
+    fontFamily: "Alegreya_500Medium",
     marginTop: 5,
-    fontSize: 12,
-    color: '#888',
+    fontSize: 14,
+    color: colors.textLight,
   },
   questRepeatability: {
+    // fontFamily: "Alegreya_400Regular",
+    fontFamily: "Alegreya_500Medium",
     marginTop: 5,
-    fontSize: 12,
-    color: '#888',
+    fontSize: 14,
+    color: colors.textLight,
   },
 });
 
