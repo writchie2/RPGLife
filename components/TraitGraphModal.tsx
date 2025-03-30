6f7ece2e199ae0b5af8a42a402fd8a3897d5ec98
djsdesigns <104704207+djsdesigns@users.noreply.github.com>
2025-03-30 10:47:11 -0500

    Implemented Trait Graph in TraitGraphModal.tsx
    - is then imported to character_main to be used

---
+++ components/TraitGraphModal.tsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { Text, StyleSheet, View, SafeAreaView } from "react-native";
+import colors from "@/constants/colors";
+
+import { useEffect } from "react";
+import { useUserData } from "@/contexts/UserContext";
+import { RadarChart } from "@salmonco/react-native-radar-chart";
+import calcEXP from "@/utils/calcEXP";
+
+const TraitGraph = () => {
+  // GET USER TRAIT DATA
+  const userData = useUserData();
+  const [levelSTR, setLevelSTR] = useState(0);
+  const [levelVIT, setLevelVIT] = useState(0);
+  const [levelAGI, setLevelAGI] = useState(0);
+  const [levelINT, setLevelINT] = useState(0);
+  const [levelCHR, setLevelCHR] = useState(0);
+
+  // When page loads, run the function to calculate the user's Level, neededEXP, and progressEXP
+  useEffect(() => {
+    const { level } = calcEXP(userData.userData?.strengthEXP || 0);
+    setLevelSTR(level);
+  }, [userData.userData?.strengthEXP]);
+
+  useEffect(() => {
+    const { level } = calcEXP(userData.userData?.vitalityEXP || 0);
+    setLevelVIT(level);
+  }, [userData.userData?.vitalityEXP]);
+
+  useEffect(() => {
+    const { level } = calcEXP(userData.userData?.staminaEXP || 0);
+    setLevelAGI(level);
+  }, [userData.userData?.staminaEXP]);
+
+  useEffect(() => {
+    const { level } = calcEXP(userData.userData?.intelligenceEXP || 0);
+    setLevelINT(level);
+  }, [userData.userData?.intelligenceEXP]);
+
+  useEffect(() => {
+    const { level } = calcEXP(userData.userData?.charismaEXP || 0);
+    setLevelCHR(level);
+  }, [userData.userData?.charismaEXP]);
+
+  // GRAPH DATA
+  const data = [
+    { label: "STR", value: levelSTR },
+    { label: "VIT", value: levelVIT },
+    { label: "INT", value: levelINT },
+    { label: "CHR", value: levelCHR },
+    { label: "AGI", value: levelAGI },
+  ];
+
+  // Compares the lvls of each trait and finds a maxValue for the graph
+  const getMAX = (lvls: Array<number>) => {
+    let max = 5;
+    for (let i = 0; i < 5; i++) {
+      while (lvls[i] > max) {
+        max += 5;
+      }
+      if (lvls[i] == max) {
+        max += 1;
+      }
+    }
+    return max;
+  };
+
+  return (
+    <View>
+      {/* // -TEST->>>>>>>>>>>>> */}
+      {/* <Text>STR: {levelSTR}</Text>
+      <Text>VIT: {levelVIT}</Text>
+      <Text>AGI: {levelAGI}</Text>
+      <Text>INT: {levelINT}</Text>
+      <Text>CHR: {levelCHR}</Text> */}
+      {/* // <<<<<<<<<<<<<-TEST- */}
+      <SafeAreaView style={styles.container}>
+        <RadarChart
+          data={data}
+          // maxValue={100}
+          maxValue={getMAX([levelSTR, levelVIT, levelAGI, levelINT, levelCHR])}
+          gradientColor={{
+            startColor: colors.graphStart,
+            endColor: colors.graphEnd,
+            count: 5,
+          }}
+          stroke={[
+            colors.graphLine,
+            colors.graphLine,
+            colors.graphLine,
+            colors.graphLine,
+            colors.textDark,
+          ]}
+          strokeWidth={[0.5, 0.5, 0.5, 0.5, 1]}
+          strokeOpacity={[1, 1, 1, 1, 0.13]}
+          labelColor={colors.text}
+          labelFontFamily="Metamorphous_400Regular"
+          dataFillColor={colors.graphStroke}
+          dataFillOpacity={0.8}
+          dataStroke={colors.graphStrokeMain}
+          dataStrokeWidth={2}
+        />
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default TraitGraph;
